fa945e53e50596730b77849ba1abe1d1b0c6b58d
Sahil Maheshwari <sahilm1711@gmail.com>
2022-07-28 10:08:23 +0530

    Creating a template

---
+++ components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <img src=""/>
+        <p>Bitcoin</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ cryptoData.js
@@ -0,0 +1,32 @@
+export const Cryptocurrencies=[
+    {
+        name: "Bitcoin(BTC)",
+        index : "$31,812.80",
+        change : "+10%",
+        price : "$60,000"
+    },
+    {
+        name: "Bitcoin(BTC)",
+        index : "$31,812.80",
+        change : "+10%",
+        price : "$60,000"
+    },
+    {
+        name: "Bitcoin(BTC)",
+        index : "$31,812.80",
+        change : "+10%",
+        price : "$60,000"
+    },
+    {
+        name: "Bitcoin(BTC)",
+        index : "$31,812.80",
+        change : "+10%",
+        price : "$60,000"
+    },
+    {
+        name: "Bitcoin(BTC)",
+        index : "$31,812.80",
+        change : "+10%",
+        price : "$60,000"
+    }
+]
