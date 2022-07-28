e65cc7d67e5afc54ae129c8e56606fe48b53054a
Sahil Maheshwari <sahilm1711@gmail.com>
2022-07-28 10:45:20 +0530

    Adding a card template

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import './Card.css'
 function Card() {
   return (
-    <div>
-        <img src=""/>
-        <p>Bitcoin</p>
+    <div class="card"> 
+    <div class="card_img"> 
+        <img src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg" alt="user-image"/>
     </div>
+    <div class="card_info">
+        <h2>USER NAME</h2>
+        <a href="#!">loremIpsum.com</a>
+    </div>
+</div>
+  
   )
 }
 
